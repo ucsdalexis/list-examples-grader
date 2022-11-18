ef659f413d79ff86737def4fff69604b3c5a27b1
Chikimiko <cococandy333@gmail.com>
2022-11-18 02:36:37 -0800

    old shit

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,58 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestListExamples {
-  // Write your grading tests here!
+    @Test
+    public void testFiller1(){
+        List<String> list = new ArrayList<>();
+	list.add("cook");
+	list.add("ate");
+	list.add("cookie");
+	
+        List<String> solution = new ArrayList<>();
+	solution.add("cook");
+	solution.add("cookie");
+	assertEquals(solution, ListExamples.filter(list, new StringCheckerClass()));
+    }
+    @Test
+    public void testFiller2(){
+        List<String> list = new ArrayList<>();
+	list.add("cook");
+	list.add("ate");
+	
+        List<String> solution = new ArrayList<>();
+	solution.add("cook");
+	assertEquals(solution, ListExamples.filter(list, new StringCheckerClass()));
+    }
+    @Test
+    public void testMerge1(){
+	List<String> list = new ArrayList<>();
+	list.add("cook");
+	list.add("ate");
+	list.add("cookie");
+	List<String> add = new ArrayList<>();
+	list.add("ate");
+	list.add("cookie");
+	List<String> solution = new ArrayList<>();
+	list.add("cook");
+	list.add("ate");
+	list.add("cookie");
+	list.add("ate");
+	list.add("cookie");
+	assertEquals(solution, ListExamples.merge(list, add));
+    }
+}
+
+class StringCheckerClass implements StringChecker {
+  
+  public StringCheckerClass() {
+
+  }
+
+  public boolean checkString(String s) {
+    if (s.length() >= 4) {return true;}
+    return false;
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,46 @@
 # Create your grading script here
 
-set -e
-
 rm -rf student-submission
 git clone $1 student-submission
 
+CP='".;lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"'
+
+
+echo 
+
+if [ -f student-submission/ListExamples.java ]
+then
+	echo "ListExamples.java at least exists"
+else
+	echo "ListExamples.java doesn't even exist"
+fi
+
+echo
+
+cp -r ./lib/ Server.java GradeServer.java TestListExamples.java ./student-submission
+
+cd ./student-submission
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+
+java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > output.txt
+
+
+
+
+SCORE=`grep 'Tests run:' output.txt` 
+
+IFS=', ' read -r -a array <<< $SCORE
+
+echo "You failed" ${array[4]} "tests out of the" ${array[2]} "tests"
+
+GRADE=${array[4]}/${array[2]}
+PASS=0.75
+
+
+if [[ $GRADE -ge $PASS ]]
+then 
+	echo "TEST"
+else
+	echo "FF"
+fi
