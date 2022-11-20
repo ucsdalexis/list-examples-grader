434b1f516fca79ce056f42e5219287d032dd252b
Chikimiko <cococandy333@gmail.com>
2022-11-20 08:18:08 -0800

    create bash script

--- TestListExamples.java
+++ TestListExamples.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestListExamples {
-    @Test
+    @Test(timeout=1000)
     public void testFiller1(){
         List<String> list = new ArrayList<>();
 	list.add("cook");
@@ -16,7 +16,8 @@ public class TestListExamples {
 	solution.add("cookie");
 	assertEquals(solution, ListExamples.filter(list, new StringCheckerClass()));
     }
-    @Test
+
+    @Test(timeout=1000)
     public void testFiller2(){
         List<String> list = new ArrayList<>();
 	list.add("cook");
@@ -26,22 +27,37 @@ public class TestListExamples {
 	solution.add("cook");
 	assertEquals(solution, ListExamples.filter(list, new StringCheckerClass()));
     }
-    @Test
+
+    @Test(timeout=1000)
     public void testMerge1(){
 	List<String> list = new ArrayList<>();
 	list.add("cook");
 	list.add("ate");
 	list.add("cookie");
-	List<String> add = new ArrayList<>();
-	list.add("ate");
-	list.add("cookie");
+	List<String> adds = new ArrayList<>();
+	adds.add("ate");
+	adds.add("cookie");
 	List<String> solution = new ArrayList<>();
+	solution.add("cook");
+	solution.add("ate");
+	solution.add("cookie");
+	solution.add("ate");
+	solution.add("cookie");
+	assertEquals(solution, ListExamples.merge(list, adds));
+    }
+
+    @Test(timeout=1000)
+    public void testMerge2(){
+	List<String> list = new ArrayList<>();
 	list.add("cook");
 	list.add("ate");
 	list.add("cookie");
-	list.add("ate");
-	list.add("cookie");
-	assertEquals(solution, ListExamples.merge(list, add));
+	List<String> adds = new ArrayList<>();
+	List<String> solution = new ArrayList<>();
+	solution.add("cook");
+	solution.add("ate");
+	solution.add("cookie");
+	assertEquals(solution, ListExamples.merge(list, adds));
     }
 }
 
--- grade.sh
+++ grade.sh
@@ -1,46 +1,51 @@
-# Create your grading script here
-
 rm -rf student-submission
 git clone $1 student-submission
 
-CP='".;lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"'
+
+CP=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" 
 
 
+# if TEST_1 = 0 then fail
+declare -i TEST_1=0
 echo 
 
 if [ -f student-submission/ListExamples.java ]
 then
-	echo "ListExamples.java at least exists"
+        echo "ListExamples.java at least exists"
+	TEST_1=1
 else
-	echo "ListExamples.java doesn't even exist"
+        echo "ListExamples.java doesn't even exist"
 fi
 
 echo
 
+
 cp -r ./lib/ Server.java GradeServer.java TestListExamples.java ./student-submission
-
 cd ./student-submission
+mkdir lib
+cp hamcrest-core-1.3.jar junit-4.13.2.jar lib
 
-javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
-
-java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > output.txt
-
+javac -cp $CP *.java
+java -cp $CP org.junit.runner.JUnitCore TestListExamples > output.txt
 
 
-
-SCORE=`grep 'Tests run:' output.txt` 
+SCORE=`grep 'Tests run:' output.txt`
 
 IFS=', ' read -r -a array <<< $SCORE
 
 echo "You failed" ${array[4]} "tests out of the" ${array[2]} "tests"
 
-GRADE=${array[4]}/${array[2]}
-PASS=0.75
+declare -i GRADE=(${array[2]}-${array[4]})*100/${array[2]}
+declare -i PASS=7*100/10
 
+echo "I guess you get a" $GRADE "% based on my grading system"
 
 if [[ $GRADE -ge $PASS ]]
-then 
-	echo "TEST"
+then
+        echo "You pass :)"
 else
-	echo "FF"
+        echo "You did not pass :("
 fi
+
+echo
+
